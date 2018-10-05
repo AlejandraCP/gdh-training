b2a0b1eabfb4cafe1f98acddb714a3ef2d0d92c5
AlejandraCP <alejandracp710@gmail.com>
2018-10-05 17:22:40 -0500

    style v1 form create-course

--- src/app/view-administrador/crear-curso/crear-curso.component.ts
+++ src/app/view-administrador/crear-curso/crear-curso.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crear-curso',
   templateUrl: './crear-curso.component.html',
-  styleUrls: ['./crear-curso.component.css']
+  styleUrls: ['./crear-curso.component.css', './../../view-user-home/view-user-home.component.css']
 })
 export class CrearCursoComponent implements OnInit {
 
   constructor() { }
 
+  date
   ngOnInit() {
+    
+  }
+
+  show() {
+    console.log(this.date)
   }
 
 }
