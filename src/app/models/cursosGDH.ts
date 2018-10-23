7a149d7c35c6259e9e97c48f5cd5e1c281bec9eb
AlejandraCP <alejandracp710@gmail.com>
2018-10-23 17:14:28 -0500

    add model to current course

---
+++ src/app/models/cursosGDH.ts
@@ -0,0 +1,12 @@
+export class CursoGDH {
+  public $key: string;
+  public dni: number;
+  public nombre: string;
+  public area: string;
+  public puesto: string;
+  public asistencia: boolean;
+  public correo: string;
+  public mesCapacitacion: string;
+  public numHoras: number;
+  public nombreCurso: string;
+}
