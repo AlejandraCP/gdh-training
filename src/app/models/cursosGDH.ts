6df2585f1ad8978cc56d9e43c59980988924ffa1
AlejandraCP <alejandracp710@gmail.com>
2018-11-02 11:19:17 -0500

    add default

--- src/app/models/cursosGDH.ts
+++ src/app/models/cursosGDH.ts
@@ -1,14 +1,14 @@
 export class UserGDH {
   public $key: string;
-  public dni: number;
+  public dni?: number;
   public nombre: string;
-  public puesto: string;
-  public departamento: string;
-  public codigo: string;
-  public vp: string;
-  public division: string
+  public puesto?: string;
+  public departamento?: string;
+  public codigo?: string;
+  public vp?: string;
+  public division?: string
   public asistencia: boolean;
-  public correo: string;
+  public correo?: string;
   public mesCapacitacion: string;
   public numHoras: number;
   public nombreCurso: string;
